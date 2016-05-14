76f8f35e1046bfda587676022a632eed0f6fb6d5
Thomas Brandl <thobra@gmail.com>
2016-05-14 14:10:07 +0200

    Refresh

--- js/main.coffee
+++ js/main.coffee
@@ -6,5 +6,10 @@ priceFormatter = (value) ->
 $(document).ready () ->
   $table = $ '#table'
 
+  $table.bootstrapTable 'refreshOptions',
+    search: true
+    showRefresh: true
+    url: './data'
+
   $('#get-data').click ->
     alert(JSON.stringify($table.bootstrapTable('getData')));
--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ priceFormatter = function(value) {
 $(document).ready(function() {
   var $table;
   $table = $('#table');
+  $table.bootstrapTable('refreshOptions', {
+    search: true,
+    showRefresh: true,
+    url: './data'
+  });
   return $('#get-data').click(function() {
     return alert(JSON.stringify($table.bootstrapTable('getData')));
   });
