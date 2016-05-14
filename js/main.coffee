b6ce9ec74b1a0df345038f5c7ad14b84508ae43b
Thomas Brandl <thobra@gmail.com>
2016-05-14 13:52:30 +0200

    getData

--- js/main.coffee
+++ js/main.coffee
@@ -4,18 +4,7 @@ priceFormatter = (value) ->
 
 
 $(document).ready () ->
-  $('#grid').w2grid(
-        name: 'grid'
-        show:
-          toolbar: on
-          toolbarSave: on
-        onSubmit: (event) -> console.log event
-        method: 'GET'
-        columns: [
-            { field: 'product', editable: {type: 'text'}, caption: 'Product', size: '30%', sortable: true }
-            { field: 'price', editable: {type: 'float'}, render: 'currency', caption: 'Price', size: '120px', sortable: true }
-        ]
-        # records: [
-        #      { product: 'TEST', price: 100.23 }
-        #  ]
-    )
+  $table = $ '#table'
+
+  $('#get-data').click ->
+    alert(JSON.stringify($table.bootstrapTable('getData')));
--- js/main.js
+++ js/main.js
@@ -5,35 +5,9 @@ priceFormatter = function(value) {
 };
 
 $(document).ready(function() {
-  return $('#grid').w2grid({
-    name: 'grid',
-    show: {
-      toolbar: true,
-      toolbarSave: true
-    },
-    onSubmit: function(event) {
-      return console.log(event);
-    },
-    method: 'GET',
-    columns: [
-      {
-        field: 'product',
-        editable: {
-          type: 'text'
-        },
-        caption: 'Product',
-        size: '30%',
-        sortable: true
-      }, {
-        field: 'price',
-        editable: {
-          type: 'float'
-        },
-        render: 'currency',
-        caption: 'Price',
-        size: '120px',
-        sortable: true
-      }
-    ]
+  var $table;
+  $table = $('#table');
+  return $('#get-data').click(function() {
+    return alert(JSON.stringify($table.bootstrapTable('getData')));
   });
 });
