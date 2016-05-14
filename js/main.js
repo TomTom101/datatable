ba8207dcda9956642849e45159be0aaaf899955e
Thomas Brandl <thobra@gmail.com>
2016-05-14 18:02:30 +0200

    Second summary table

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var groupedData, priceFormatter, sumFormatter;
+var $categories, $products, groupedData, priceFormatter, sumFormatter;
 
 priceFormatter = function(value) {
   return Math.round(value);
@@ -6,25 +6,45 @@ priceFormatter = function(value) {
 
 sumFormatter = function(data) {
   var sum;
-  sum = _.chain(data).reduce(function(memo, data) {
-    return memo + parseFloat(data.price);
-  }, 0.0).value();
+  sum = _.chain(data).reduce((function(_this) {
+    return function(memo, data) {
+      return memo + parseFloat(data[_this.field]);
+    };
+  })(this), 0.0).value();
   return Math.floor(sum / 500) * 500;
 };
 
 groupedData = function(data) {
-  return console.log(data);
+  var categories;
+  categories = _.chain(data).groupBy(function(data) {
+    return data.product;
+  }).map(function(data) {
+    return _.reduce(data, function(memo, set) {
+      return {
+        category: set.product,
+        sum: parseFloat(set.price) + memo.sum
+      };
+    }, {
+      sum: 0
+    });
+  }).value();
+  return $categories.bootstrapTable({
+    data: categories
+  });
 };
 
+$products = $('#products');
+
+$categories = $('#categories');
+
+$products.bootstrapTable({
+  onLoadSuccess: groupedData
+});
+
 $(document).ready(function() {
-  var $table;
-  $table = $('#table');
-  $table.bootstrapTable({
-    onLoadSuccess: groupedData
-  });
   return $('#get-data').click(function() {
     var data;
-    data = $table.bootstrapTable('getData');
+    data = $products.bootstrapTable('getData');
     return alert(JSON.stringify(data));
   });
 });
