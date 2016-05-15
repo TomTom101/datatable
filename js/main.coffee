0a6eb99775413c257f29776262457268d248d0c0
Thomas Brandl <thobra@gmail.com>
2016-05-15 15:22:35 +0200

    Ignore selected rows in summary

--- js/main.coffee
+++ js/main.coffee
@@ -1,17 +1,29 @@
+$products = $ '#products'
+$categories = $ '#categories'
 
 priceFormatter = (value) ->
   Math.round value
 
+dailyFormatter = (value) ->
+  Math.round value * 10
+
 sumFormatter = (data) ->
+  #console.log @formatter
   sum = _.chain data
     .reduce (memo, data) =>
-      memo + parseFloat(data[@field])
+      value = window[@formatter]? data[@field]
+      value = data[@field] if !value?
+      memo + parseFloat value
     , 0.0
     .value()
   Math.floor(sum/500) * 500
 
-groupedData = (data) ->
-  categories = _.chain data
+groupedData = (data, filter) ->
+  # before we can use load, we need to set it up once
+  $categories.bootstrapTable data: newData unless filter
+  newData =
+    _.chain data
+    .filter filter
     .groupBy (data) -> data.product
     .map (data) ->
       _.reduce data, (memo, set) ->
@@ -19,14 +31,18 @@ groupedData = (data) ->
         sum: parseFloat(set.price) + memo.sum
       , sum: 0
     .value()
-  $categories.bootstrapTable data: categories
+  $categories.bootstrapTable 'load', newData
 
-
-$products = $ '#products'
-$categories = $ '#categories'
+onCheck = ->
+  allData = $products.bootstrapTable 'getData'
+  groupedData allData, (data) ->
+    !data.state
 
 $products.bootstrapTable
   onLoadSuccess: groupedData
+  onCheck: onCheck
+  onUncheck: onCheck
+  onCheckSome: () -> console.log "check some"
 
 $(document).ready () ->
 
--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
-var $categories, $products, groupedData, priceFormatter, sumFormatter;
+var $categories, $products, dailyFormatter, groupedData, onCheck, priceFormatter, sumFormatter;
+
+$products = $('#products');
+
+$categories = $('#categories');
 
 priceFormatter = function(value) {
   return Math.round(value);
+};
+
+dailyFormatter = function(value) {
+  return Math.round(value * 10);
 };
 
 sumFormatter = function(data) {
   var sum;
   sum = _.chain(data).reduce((function(_this) {
     return function(memo, data) {
-      return memo + parseFloat(data[_this.field]);
+      var name, value;
+      value = typeof window[name = _this.formatter] === "function" ? window[name](data[_this.field]) : void 0;
+      if (value == null) {
+        value = data[_this.field];
+      }
+      return memo + parseFloat(value);
     };
   })(this), 0.0).value();
   return Math.floor(sum / 500) * 500;
 };
 
-groupedData = function(data) {
-  var categories;
-  categories = _.chain(data).groupBy(function(data) {
+groupedData = function(data, filter) {
+  var newData;
+  if (!filter) {
+    $categories.bootstrapTable({
+      data: newData
+    });
+  }
+  newData = _.chain(data).filter(filter).groupBy(function(data) {
     return data.product;
   }).map(function(data) {
     return _.reduce(data, function(memo, set) {
@@ -28,17 +46,24 @@ groupedData = function(data) {
       sum: 0
     });
   }).value();
-  return $categories.bootstrapTable({
-    data: categories
+  return $categories.bootstrapTable('load', newData);
+};
+
+onCheck = function() {
+  var allData;
+  allData = $products.bootstrapTable('getData');
+  return groupedData(allData, function(data) {
+    return !data.state;
   });
 };
 
-$products = $('#products');
-
-$categories = $('#categories');
-
 $products.bootstrapTable({
-  onLoadSuccess: groupedData
+  onLoadSuccess: groupedData,
+  onCheck: onCheck,
+  onUncheck: onCheck,
+  onCheckSome: function() {
+    return console.log("check some");
+  }
 });
 
 $(document).ready(function() {
